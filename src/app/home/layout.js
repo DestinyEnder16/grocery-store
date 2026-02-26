4d82ead700243866715ba38e250644cbdf4df723
Israel Destiny <destinyprogrammer06@gmail.com>
2026-02-27 00:50:26 +0100

    Drawer navigation has been built

--- src/app/home/layout.js
+++ src/app/home/layout.js
@@ -1,5 +1,13 @@
+import ProfileDrawer from '@/src/components/Drawer';
 import MyTabBar from '@/src/components/TabNav';
+import { Cart, FavoriteSvg, Home, UserSvg } from '@/src/types';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import CartScreen from './screens/cart';
 import FavoriteScreen from './screens/favorite';
@@ -7,6 +15,81 @@ import Homepage from './screens/homepage';
 import ProfileScreen from './screens/profile';
 
 const Tab = createBottomTabNavigator();
+
+const Drawer = createDrawerNavigator();
+
+const styles = StyleSheet.create({
+  txt: {
+    fontFamily: 'Poppins-Font',
+    // color: 'grey',
+    fontSize: 18,
+  },
+  h3: {
+    fontFamily: 'Poppins-Font-Semibold',
+    fontSize: 24,
+  },
+});
+
+const ProfileScreenWithDrawer = () => {
+  return (
+    <Drawer.Navigator
+      screenOptions={{ headerShown: false }}
+      drawerContent={(props) => (
+        <DrawerContentScrollView>
+          <View onPress={() => props.navigation.closeDrawer()}>
+            {/* <ProfilePicture /> */}
+            <ProfileDrawer
+              fn={() =>
+                props.navigation.navigate('MainTabs', { screen: 'Profile' })
+              }
+            />
+            <Text style={styles.h3}>Destiny</Text>
+          </View>
+
+          <DrawerItem
+            label="Home"
+            icon={() => <Home />}
+            activeTintColor="black"
+            labelStyle={styles.txt}
+            onPress={() =>
+              props.navigation.navigate('MainTabs', { screen: 'Home' })
+            }
+          />
+
+          <DrawerItem
+            label="Cart"
+            icon={() => <Cart />}
+            activeTintColor="black"
+            labelStyle={styles.txt}
+            onPress={() =>
+              props.navigation.navigate('MainTabs', { screen: 'Cart' })
+            }
+          />
+          <DrawerItem
+            label="Favorite"
+            icon={() => <FavoriteSvg />}
+            activeTintColor="black"
+            labelStyle={styles.txt}
+            onPress={() =>
+              props.navigation.navigate('MainTabs', { screen: 'Favorite' })
+            }
+          />
+          <DrawerItem
+            label="Profile"
+            icon={() => <UserSvg />}
+            activeTintColor="black"
+            labelStyle={styles.txt}
+            onPress={() =>
+              props.navigation.navigate('MainTabs', { screen: 'Profile' })
+            }
+          />
+        </DrawerContentScrollView>
+      )}
+    >
+      <Drawer.Screen name="MainTabs" component={RootTabs} />
+    </Drawer.Navigator>
+  );
+};
 
 function RootTabs() {
   return (
@@ -30,7 +113,7 @@ function RootTabs() {
 export default function HomeLayout() {
   return (
     <SafeAreaProvider>
-      <RootTabs />
+      <ProfileScreenWithDrawer />
     </SafeAreaProvider>
   );
 }
---
+++ src/components/ProfilePicture.tsx
@@ -0,0 +1,25 @@
+import { Image } from 'expo-image';
+import { StyleSheet, View } from 'react-native';
+
+export default function ProfilePicture() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source="@/assets/images/planet-pfp.jpg"
+        style={[styles.pfp]}
+        // width={'100%'}
+        contentFit="cover"
+        transition={200} // Smooth fade-in so it doesn't "pop"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 2, marginBottom: 10 },
+  pfp: {
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+  },
+});
