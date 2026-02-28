897a2418d880e844977ba9909b9d32a9685d1504
Israel Destiny <destinyprogrammer06@gmail.com>
2026-02-28 06:17:19 +0100

    redundant style removed

--- src/app/home/screens/homepage.tsx
+++ src/app/home/screens/homepage.tsx
@@ -1,22 +1,5 @@
 import ProfileDrawer from '@/src/components/ProfileDrawer';
 import ScreenView from '@/src/components/ScreenView';
-import { StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    justifyContent: 'space-between',
-  },
-  view: {
-    alignSelf: 'center',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  pagetxt: {
-    fontFamily: 'Poppins-Font',
-    fontSize: 24,
-  },
-});
 
 export default function Homepage() {
   return (
